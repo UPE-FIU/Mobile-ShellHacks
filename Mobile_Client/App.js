d6152a2c2fdb3a9437428e9b0c50b53762298d74
jehfkemsy <jehf.denezaire@gmail.com>
2019-05-02 16:34:33 -0400

    fixing some stuff

--- Mobile_Client/App.js
+++ Mobile_Client/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { StyleSheet, Platform, AsyncStorage } from "react-native";
 import AppNavigator from "./src/navigation";
-import axios from 'axios';
-import LocalStorage from './src/services/context'
+import LocalStorage from './src/services/context';
 
 export default class App extends React.Component {
   render() {
